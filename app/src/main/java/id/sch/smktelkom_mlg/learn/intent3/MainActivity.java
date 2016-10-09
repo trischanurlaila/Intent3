550e2d973845026ff34e181dac33a776aec02f22
TrischaNL <l.trischa@gmail.com>
2016-10-09 03:43:31 -0700

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -38,7 +40,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ImageView ivCamera = (ImageView) findViewById(R.id.imageViewCamera);
+        ivCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capturePhoto();
+            }
+        });
+
         
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void capturePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager())!=null)
+            startActivityForResult(intent,  REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK)
+        {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
     }
 
     public void openWebPage(String url)
